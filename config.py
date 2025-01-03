9b376d13498ac4e0eed9eb6afc952b43854f80d3
alokranjanarteez <alok.ranjan@arteezhr.com>
2025-01-04 03:11:13 +0530

    adding streamlit variable

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+os.environ['GROQ_API_KEY'] =  st.secrets['GROQ_API_KEY'] 
 
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL_NAME = "llama-3.3-70b-versatile"
 TEMPERATURE = 0
 MAX_RETRIES = 2
