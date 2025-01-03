b4de22507e641e143eb12c9d0adb67e08b0a58ec
alokranjanarteez <alok.ranjan@arteezhr.com>
2025-01-04 03:32:44 +0530

    correfcting secret.toml

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 import streamlit as st
 
-os.environ['GROQ_API_KEY'] =  st.secrets['GROQ_KEY'] 
+GROQ_API_KEY =  st.secrets['GROQ_KEY'] 
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL_NAME = "llama-3.3-70b-versatile"
 TEMPERATURE = 0
 MAX_RETRIES = 2
